5ad4c30582cd3d517e5a2f1fb2c9e0f4b39b26cb
lukelong <1258828871@qq.com>
2021-03-04 17:13:54 +0800

    意见反馈,实战项目基本结束

--- pages/feedback/feedback.js
+++ pages/feedback/feedback.js
@@ -5,62 +5,40 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    tabs: [{
+      id: 0,
+      value: '体验问题',
+      isActive: true
+    }, {
+      id: 1,
+      value: '商品,商家投诉',
+      isActive: false
+    }],
+    chooseImages:[]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleTabsItemChange(e) {
+    const index = e.detail.index;
+    //修改原数组
+    let tabs = this.data.tabs;
+    tabs.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false);
+    this.setData({
+      tabs
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleChooseImg() {
+    wx.chooseImage({
+      count: 9,
+      sizeType:['original','compressed'],
+      sourceType:['album','camera'],
+      success:(result)=>{
+        this.setData({chooseImages:[...this.data.chooseImages,...result.tempFilePaths]});
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleRemoveImg(e){
+    const index = e.currentTarget.dataset.index;
+    let {chooseImages} = this.data;
+    chooseImages.splice(index,1);
+    this.setData({chooseImages});
   }
 })
--- pages/search/search.js
+++ pages/search/search.js
@@ -1,3 +1,5 @@
+import {request} from "../../request/index.js";
+
 // pages/search/search.js
 Page({
 
@@ -5,62 +7,35 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    goods:[],
+    isFocus:false,
+    inputValue:""
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  TimeId:-1,
+  //监听文本框输入事件
+  handleInput(e){
+    //获取输入框的值
+    const {value} = e.detail;
+    if(!value.trim()){
+      return;
+    }
+    clearTimeout(this.TimeId);
+    this.TimeId = setTimeout(() => {
+      this.setData({isFocus:true});
+      this.qsearch(value);
+    }, 500);
+  }, 
+  handleCancel(){
+    this.setData({
+      isFocus:false,
+      inputValue:"",
+      goods:[]
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+  async qsearch(query){
+    const res = await request({url:"https://api-hmugo-web.itheima.net/api/public/v1/goods/qsearch",data:{query}});
+    this.setData({
+      goods:res.data.message
+    })
+  } 
 })
